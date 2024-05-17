ecd4b1327d3d81741f3131ddc3ff3abc0a071965
Michele Zanotti <m.zanotti@nebuly.ai>
2024-05-17 12:08:50 +0200

    fix typo

--- main.tf
+++ main.tf
@@ -260,7 +260,7 @@ module "eks_iam_role" {
   attach_external_secrets_policy                     = true
   external_secrets_secrets_manager_create_permission = true
   attach_ebs_csi_policy                              = true
-  attach_fbs_csi_policy                              = true
+  attach_efs_csi_policy                              = true
 
   # TODO - it's likely that we don't need these if we use 
   # EKS managed node groups
