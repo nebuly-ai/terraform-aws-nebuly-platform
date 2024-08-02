3d639316c8841d40319f283ab036275620266745
Michele Zanotti <m.zanotti@nebuly.ai>
2024-08-02 13:12:09 +0200

    chore: update backend

--- tests/dev-provisioning/remote_state.tf
+++ tests/dev-provisioning/remote_state.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-shared"
     storage_account_name = "nbllabtfstatessa"
-    container_name       = "platform-aws-tfstate"
+    container_name       = "int-test-platform-aws-tfstate"
     key                  = "tfstate"
   }
 }
